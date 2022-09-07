e2427189ff874be984cff1ffbbbb88077101d635
Uliana Le <ulianochka29@mail.ru>
2022-09-07 05:55:51 +0300

    add files

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Здравствуйте!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Здравствуйте!"))
+        ;
+    }
+
+}
